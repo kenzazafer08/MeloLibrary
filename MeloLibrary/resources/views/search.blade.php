5fb2fdbc6f29fefc2d78f824c502ade421dad73b
unknown <zaaferkenza08@gmail.com>
2023-03-21 00:15:10 +0000

    songs cards

--- MeloLibrary/resources/views/search.blade.php
+++ MeloLibrary/resources/views/search.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
     @include('inc.sidebar')
-    <div class="p-4 sm:ml-64 mt-20">
+    <div class="p-4 lg:ml-64 mt-20">
             <form>
                 <div class="flex">
-                    <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+                    <label for="search-dropdown" class="mb-2 text-lg font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
                     <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">All categories <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
                     <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
@@ -32,37 +32,37 @@
                 </div>
             </form>           
         <div class="flex flex-wrap my-10 px-10 w-full justify-around "> 
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Podcast</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Tv</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Latino</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Gaming</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Hindi</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Arabic</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Summer</h5>
-                    <p></p>
+                    
                 </div>
-                <div class="mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-center items-center mb-4 bg-red-800 h-32 w-48 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
                     <h5 class="text-center p-4 text-xl font-semibold tracking-tight text-white">Night</h5>
-                    <p></p>
+                    
                 </div>
         </div>         
     </div>    
--- MeloLibrary/resources/views/welcome.blade.php
+++ MeloLibrary/resources/views/welcome.blade.php
@@ -7,109 +7,126 @@
             <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
                 <div class="relative h-2/3">
                     <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
-                  
-                    <a href="#" class="absolute bottom-0 left-0 m-4">
+                    <button onclick="playA('my-audio')" id='play-A' class="absolute bottom-0 left-0 m-4">
                       <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
                         <path fill="none" d="M0 0h48v48H0z"></path>
                         <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
                       </svg>
-                    </a>
+                      <audio class="hidden" id="my-audio" src="{{ asset('audios/killpill.mp3') }}"></audio>
+                    </button>
                   </div>                  
                 <div class="p-5">
                     <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
                     <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
-                    
                 </div>
             </div>
+
             <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
-                <div class="relative h-2/3">
-                    <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
-                  
-                    <a href="#" class="absolute bottom-0 left-0 m-4">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
-                        <path fill="none" d="M0 0h48v48H0z"></path>
-                        <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
-                      </svg>
-                    </a>
-                  </div>                  
-                <div class="p-5">
-                    <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
-                    <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
-                    
-                </div>
+              <div class="relative h-2/3">
+                  <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
+                  <button onclick="playA('my-audio')" id='play-A' class="absolute bottom-0 left-0 m-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
+                      <path fill="none" d="M0 0h48v48H0z"></path>
+                      <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
+                    </svg>
+                    <audio class="hidden" id="my-audio" src="{{ asset('audios/killpill.mp3') }}"></audio>
+                  </button>
+                </div>                  
+              <div class="p-5">
+                  <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
+                  <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
+              </div>
+          </div>
+          <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
+            <div class="relative h-2/3">
+                <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
+                <button onclick="playA('my-audio')" id='play-A' class="absolute bottom-0 left-0 m-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                    <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
+                  </svg>
+                  <audio class="hidden" id="my-audio" src="{{ asset('audios/killpill.mp3') }}"></audio>
+                </button>
+              </div>                  
+            <div class="p-5">
+                <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
+                <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
             </div>
-            <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
-                <div class="relative h-2/3">
-                    <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
-                  
-                    <a href="#" class="absolute bottom-0 left-0 m-4">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
-                        <path fill="none" d="M0 0h48v48H0z"></path>
-                        <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
-                      </svg>
-                    </a>
-                  </div>                  
-                <div class="p-5">
-                    <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
-                    <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
-                    
-                </div>
-            </div>
+        </div>
         </div>
         <p class="font-bold text-xl px-4">Most Liked</p>
         <div class="flex flex-wrap my-10 px-10 w-full justify-around">   
-            <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
-                <div class="relative h-2/3">
-                    <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
-                  
-                    <a href="#" class="absolute bottom-0 left-0 m-4">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
-                        <path fill="none" d="M0 0h48v48H0z"></path>
-                        <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
-                      </svg>
-                    </a>
-                  </div>                  
-                <div class="p-5">
-                    <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
-                    <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
-                    
-                </div>
-            </div>
-            <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
-                <div class="relative h-2/3">
-                    <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
-                  
-                    <a href="#" class="absolute bottom-0 left-0 m-4">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
-                        <path fill="none" d="M0 0h48v48H0z"></path>
-                        <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
-                      </svg>
-                    </a>
-                  </div>                  
-                <div class="p-5">
-                    <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
-                    <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
-                    
-                </div>
-            </div>
-            <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
-                <div class="relative h-2/3">
-                    <img class="rounded-t-lg w-full h-full" src="{{asset('images/killpil.jfif')}}" alt="" />
-                  
-                    <a href="#" class="absolute bottom-0 left-0 m-4">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
-                        <path fill="none" d="M0 0h48v48H0z"></path>
-                        <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
-                      </svg>
-                    </a>
-                  </div>                  
-                <div class="p-5">
-                    <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">Kill Pill</h5>
-                    <p class="font-normal text-sm text-gray-700 ">- By SZA</p>
-                    
-                </div>
+          <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
+            <div class="relative h-2/3">
+                <img class="rounded-t-lg w-full h-full" src="{{asset('images/dross.jfif')}}" alt="" />
+              
+                <button onclick="playA('test')" class="absolute bottom-0 left-0 m-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                    <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
+                  </svg>
+                  <audio class="hidden" id="test" src="{{ asset('audios/m3a3chran.mp3') }}"></audio>
+                </button>
+              </div>                  
+            <div class="p-5">
+                <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">M3a 3echrane</h5>
+                <p class="font-normal text-sm text-gray-700 ">- Dizzy Dross</p>
+                
             </div>
         </div>
+        <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
+          <div class="relative h-2/3">
+              <img class="rounded-t-lg w-full h-full" src="{{asset('images/dross.jfif')}}" alt="" />
+            
+              <button onclick="playA('test')" class="absolute bottom-0 left-0 m-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
+                  <path fill="none" d="M0 0h48v48H0z"></path>
+                  <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
+                </svg>
+                <audio class="hidden" id="test" src="{{ asset('audios/m3a3chran.mp3') }}"></audio>
+              </button>
+            </div>                  
+          <div class="p-5">
+              <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">M3a 3echrane</h5>
+              <p class="font-normal text-sm text-gray-700 ">- Dizzy Dross</p>
+              
+          </div>
+      </div>
+      <div class="w-72 h-80 bg-white border border-gray-200 rounded-lg shadow ">
+        <div class="relative h-2/3">
+            <img class="rounded-t-lg w-full h-full" src="{{asset('images/dross.jfif')}}" alt="" />
+          
+            <button onclick="playA('test')" class="absolute bottom-0 left-0 m-4">
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current text-white">
+                <path fill="none" d="M0 0h48v48H0z"></path>
+                <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
+              </svg>
+              <audio class="hidden" id="test" src="{{ asset('audios/m3a3chran.mp3') }}"></audio>
+            </button>
+          </div>                  
+        <div class="p-5">
+            <h5 class="text-2xl font-bold tracking-tight text-gray-900 ">M3a 3echrane</h5>
+            <p class="font-normal text-sm text-gray-700 ">- Dizzy Dross</p>
+            
+        </div>
     </div>
-        
+            </div>
+
+
+        </div>
+    </div>
+    <script>
+      function playA(id){
+        console.log('test')
+        const playButtons = document.querySelectorAll('.fill-current');
+              const audio = document.getElementById(id);
+              if (audio.paused) {
+                audio.play();
+              } else {
+                audio.pause();
+              }
+            }
+</script>
+
 @endsection
+
