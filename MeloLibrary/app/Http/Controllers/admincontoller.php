d45ebf4975ae2cf372917d4671532b80c56ba3c3
unknown <zaaferkenza08@gmail.com>
2023-03-25 17:01:41 +0000

    admin statistics

--- MeloLibrary/app/Http/Controllers/admincontoller.php
+++ MeloLibrary/app/Http/Controllers/admincontoller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\artist;
+use App\Models\categorie;
 use Illuminate\Http\Request;
 use App\Models\User;
 class admincontoller extends Controller
@@ -11,6 +12,7 @@ class admincontoller extends Controller
     {
         $users = User::where('role', 0)->count();
         $artists = artist::count();
-        return view('admin.admin', compact('users','artists'));
+        $categorie = categorie::count();
+        return view('admin.admin', compact('users','artists','categorie'));
     }
 }
