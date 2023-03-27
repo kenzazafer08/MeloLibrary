e22af9a5420fa0b6fc0a62eb77f465177930dc65
unknown <zaaferkenza08@gmail.com>
2023-03-27 14:50:55 +0000

    audio

--- MeloLibrary/resources/views/detail.blade.php
+++ MeloLibrary/resources/views/detail.blade.php
@@ -1,28 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <div class="p-4 sm:ml-64 mt-20">
-<div class="flex justify-between">
+<div class="flex justify-between items-center">
     <div class="flex flex-col items-center w-1/3">
     <div class="flex items-center jusify-between">
     <div>
+    <div class="flex flex-col items-center">
         <img class=" w-48 rounded-full" src="{{asset('uploads')}}/{{$song->Image}}"/>
     <h5 class="py-4 font-bold text-red-700 text-lg text-center">{{$song->Titre}}</h5>
-    <div class="flex justify-around items-center">
-        <button onclick="playA({{$song->id}})" class="">
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-current">
-              <path fill="none" d="M0 0h48v48H0z"></path>
-              <path width="40" height="40" d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z" fill="red"></path>
-            </svg>
-            <audio class="hidden" id="{{$song->id}}" src="{{ asset('uploads') }}/{{$song->audio}}"></audio>
-        </button>
+    {{-- <audio class="" id="{{$song->id}}" src="{{ asset('uploads') }}/{{$song->audio}}"></audio> --}}
+    <audio controls>
+        <source src="{{ asset('uploads') }}/{{$song->audio}}" type="audio/mpeg">
+      </audio>
+    </div>
+    <div class="flex justify-center items-center mt-4">
         @if($like == 1)
-            <a href="{{route('like.song',$song->id)}}">
+            <p class="flex justify-center items-center">{{$count_like}}<a href="{{route('like.song',$song->id)}}">
                 <svg width="40px" height="40px" viewBox="0 0 24 24" fill="red" class="text-red-700" xmlns="http://www.w3.org/2000/svg">
                     <g id="Interface / Heart_01">
                     <path id="Vector" d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </g>
                 </svg>
-            </a> {{$count_like}}
+            </a> </p>
         @else
         <a href="{{route('like.song',$song->id)}}">
             <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" class="text-red-700" xmlns="http://www.w3.org/2000/svg">
@@ -47,10 +46,9 @@
     </div>
     </div>
     </div>
-    <div class="flex flex-wrap">
-    @if($artists)<div>
-        
-        <h1 class="font-extrabold text-2xl text-center">Artists</h1>
+    <div class="flex flex-wrap justify-center">
+        <div>
+        <h1 class="font-extrabold text-2xl text-center">Artists & bands</h1>
         <div class="flex flex-wrap">
         @foreach ($artists as $artist)
             <span class="px-4 py-2 text-red-900 font-bold text center">
@@ -59,13 +57,6 @@
 
             </span>
         @endforeach
-
-        </div>
-    </div> @endif      
-    @if($bands) <div> 
-            
-        <h1 class="font-extrabold text-2xl text-center">Bands</h1>
-        <div class="flex flex-wrap">
         
         @foreach ($bands as $band)
         <span class="px-4 py-2 text-red-900 font-bold text center">
@@ -76,7 +67,7 @@
         @endforeach
         
         </div>
-    </div>@endif
+    </div>
     @if($categories)
     <div>
         <h1 class="font-extrabold text-2xl text-center">Categories</h1>
